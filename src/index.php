71da73c7dbb4895eec90ff29a44b18cf52fdda6e
msDekova <msdekova1998@gmail.com>
2022-12-01 15:36:42 +0200

    Connect database

--- src/index.php
+++ src/index.php
@@ -1,3 +1,14 @@
+<?php
+  // Create connection
+  $conn = mysqli_connect('mysql:host=mysql8-service;port=3306;dbname=db_name', 'root', 'password');
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+?>
+
 <html>
  <head>
   <title>PHP Test</title>
