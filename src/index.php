b2ac9dd119aaa8e175dd337fb97c8ec048e5b3bd
msDekova <msdekova1998@gmail.com>
2022-12-01 21:08:09 +0200

    Connect to db

--- src/index.php
+++ src/index.php
@@ -1,11 +1,5 @@
 <?php
-  // Create connection
-  try {
-    $conn = new mysqli("mysql8-service", "root", "password", "db_name");
-    echo "Connected successfully";
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
+  $con= new mysqli('mysql8-service', 'root', 'password', 'db_name')or die("Could not connect to mysql".mysqli_error($con));
 ?>
 
 <html>
