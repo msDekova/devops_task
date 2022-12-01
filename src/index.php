8f6027588a83a65fd0d257c943f4f83582fa50cf
msDekova <msdekova1998@gmail.com>
2022-12-01 15:54:05 +0200

    Create new mysqli

--- src/index.php
+++ src/index.php
@@ -1,12 +1,6 @@
 <?php
   // Create connection
-  $conn = mysqli_connect('mysql:host=mysql8-service;port=3306;dbname=db_name', 'root', 'password');
-
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-  echo "Connected successfully";
+  $con= new mysqli('mysql:host=mysql8-service;port=3306;dbname=db_name', 'root', 'password')or die("Could not connect to mysql".mysqli_error($con));
 ?>
 
 <html>
