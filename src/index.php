79e17766bf8257a5743e30f75d282c486ab72aad
msDekova <msdekova1998@gmail.com>
2022-12-02 11:28:54 +0200

    Remove db_name from mysql connection

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-  $con= new mysqli('mysql8-service', 'root', 'password', 'db_name')or die("Could not connect to mysql".mysqli_error($con));
+  $con= new mysqli('mysql8-service', 'root', 'password')or die("Could not connect to mysql".mysqli_error($con));
 ?>
 
 <html>
